77a8fa58375eda8c64f03c77e045cac02f206549
rafael marca <rmarca@marca.(none)>
2012-10-27 09:11:44 -0200

    Meu primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RafaelFinancas::Application.config.secret_token = '47f345087c7a835dd9402a339536e8fe669b5f8bd9db2c747207e8bc501bb1cd49e711a328dac15f3b421db84244d574eaecbf2e4e097cc8602acf9040de4acb'
